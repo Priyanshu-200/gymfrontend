9d2909fa384725e7cd7f96fd04666dba16582b9b
Priyanshu <sontakkepriyanshu1@gmail.com>
2024-06-05 18:59:42 +0530

    new update

---
+++ src/components/WorkoutSessions.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const WorkoutSessions = () => {
+  return (
+    <section className='workout_session'>
+      <div className="wrapper">
+        <h1>TOP WORKOUT SESSION</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores aspernatur maxime aut soluta! Saepe et cumque natus asperiores!
+        </p>
+        <img src="/img5.jpg" alt="workout" />
+      </div>
+      <div className="wrapper">
+        <h1>FEATURED BOOTCAMPS</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus facilis blanditiis suscipit magnam iusto, aperiam in obcaecati cumque.
+        </p>
+        <div className="bootcamps">
+          <div>
+            <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum excepturi provident beatae placeat porro incidunt!</p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum excepturi provident beatae placeat porro incidunt!</p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum excepturi provident beatae placeat porro incidunt!</p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum excepturi provident beatae placeat porro incidunt!</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default WorkoutSessions
